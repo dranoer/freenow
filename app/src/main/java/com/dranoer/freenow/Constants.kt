93e5d97f3571a1c3db2fb2c9e648d63d0f50f5e3
dranoer <dranoer@gmail.com>
2022-01-16 18:46:10 +0330

    add: Constants

---
+++ app/src/main/java/com/dranoer/freenow/Constants.kt
@@ -0,0 +1,12 @@
+package com.dranoer.freenow
+
+object Constants {
+
+    const val BASE_URL = "https://fake-poi-api.mytaxi.com"
+    const val TIME_OUT = 30L
+
+    const val LATITUDE_ONE = 53.694865
+    const val LONGITUDE_ONE = 9.757589
+    const val LATITUDE_TWO = 53.394655
+    const val LONGITUDE_TWO = 10.099891
+}
