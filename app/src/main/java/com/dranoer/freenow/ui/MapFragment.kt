30f61ae915c6db85c52b6ea3c47ea06a54b955aa
dranoer <dranoer@gmail.com>
2022-01-16 23:45:43 +0330

    add: Map screen
    Handling location permission still remains

---
+++ app/src/main/java/com/dranoer/freenow/ui/MapFragment.kt
@@ -0,0 +1,37 @@
+package com.dranoer.freenow.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.dranoer.freenow.Constants
+import com.dranoer.freenow.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapFragment : Fragment() {
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        val sydney = LatLng(Constants.LATITUDE_ONE, Constants.LONGITUDE_ONE)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
